734d6c7865fc4679b9859913820359abd84111a8
admin <aqeelmemon46@gmail.com>
2024-06-20 11:33:01 +0530

    take input in java

--- src/com/tnsif/programs/EncapsulationDemo.java
+++ src/com/tnsif/programs/EncapsulationDemo.java
@@ -1,5 +1,11 @@
 package com.tnsif.programs;
 
 public class EncapsulationDemo {
-
+	public static void main(String[]args) {
+		//TODO Auto-generated stub
+		
+		Encapsulation obj = new Encapsulation();
+		
+		obj.
+	}
 }
---
+++ src/tns_20_6_24/scann.java
@@ -0,0 +1,11 @@
+package tns_20_6_24;
+import java.util.Scanner;
+
+public class scann {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String n = input.nextLine();
+		System.out.println("Hello"+" "+n);
+	}
+}

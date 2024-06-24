4137dac2049b413767585bfcfbe3958cdff6d725
admin <aqeelmemon46@gmail.com>
2024-06-24 22:08:13 +0530

    bvjhyg

--- src/assignments_3/Main.java
+++ src/assignments_3/Main.java
@@ -2,10 +2,10 @@ package assignments_3;
 
 public class Main {
 	 public static void main(String[] args) {
-	        // Create an instance of State
+	       
 	        State state = new State("Asia", 4500, "India", 29, "Karnataka", 61);
 
-	        // Display information
+	        
 	        state.displayInfo();
 	    }
 }

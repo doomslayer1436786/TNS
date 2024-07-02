e2a1c8633ab4b8dd325741de0522dca02e6e864a
admin <aqeelmemon46@gmail.com>
2024-07-02 11:59:05 +0530

    ass_7

---
+++ src/assignment_7/Main.java
@@ -0,0 +1,14 @@
+package assignment_7;
+
+public class Main {
+	
+	public static void main (String [] args) {
+		
+		ConstructorOverloading c1 = new ConstructorOverloading();
+		ConstructorOverloading c2 = new ConstructorOverloading("Constructor 2 called");
+		ConstructorOverloading c3 = new ConstructorOverloading("Constructor 3 called with integer value ",245);
+		
+	
+	}
+
+}
